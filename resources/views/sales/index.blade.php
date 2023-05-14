a1584dd0e6f8da0c153a4f4bc0e68d6b0f719907
SayraLizarraga <126122923+SayraLizarraga@users.noreply.github.com>
2023-05-14 16:02:26 -0600

    sales

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12 col-md-9 col-lg-6">
-            <a href="{{ route('sales.create') }}" class="btn btn-lg btn-primary"> Create Superpower</a>
+            <a href="{{ route('sales.create') }}" class="btn btn-lg btn-primary"> Create Order</a>
         </div>
     </div>
 </div>
